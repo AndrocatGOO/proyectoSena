28338489d57a3a5cb0959d3fdcbefcd87535d613
EdwinRojasVargas <edwinestudio72@gmail.com>
2021-03-28 15:04:04 -0500

    Cree base para validacion de imputs de register

--- scripts/login.js
+++ scripts/login.js
@@ -1,4 +1,6 @@
-//validacion login
+// Va lidacion del Register.  Perdon si sale un poco mal att: Cocho
 
-
-
+function validarRegister(){
+    var nombre, apellidos, correo, usuario, contraseña, RepiteContra, expresion;
+    nombre = document.getElementById
+}
