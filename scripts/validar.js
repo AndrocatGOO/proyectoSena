fcf9732749788a31eeddfa31c4cea17f222b8471
AndrocatGOO <43019783+AndrocatGOO@users.noreply.github.com>
2021-03-30 17:15:42 -0500

    arregle algo de escritura y explique a cocho

--- scripts/validar.js
+++ scripts/validar.js
@@ -1,20 +1,20 @@
 
 function validar(){
-    var nombre
-    var apellidos
-    var correo
-    var usuario
-    var contraseña
-    var RepiteContra
+    var nombre;
+    var apellidos;
+    var correo;
+    var usuario;
+    var clave;
+    var confirmar_clave;
     var expresion;
     nombre = document.getElementById("nombre").value;
     apellidos= document.getElementById("apellidos").value;
     correo = document.getElementById("correo").value;
     usuario = document.getElementById("usuario").value;
     clave = document.getElementById("clave").value;
-    Confirmar_clave = document.getElementById("Confirmar_clave").value;
+    confirmar_clave = document.getElementById("confirmar_clave").value;
 
-    if(nombre === " "){
+    if(nombre === ""){
         alert("El campo nombre esta vacio");
         return false;
     }
