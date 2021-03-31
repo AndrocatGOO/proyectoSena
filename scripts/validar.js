bc82bd144cbd7d0b65fdec4fd72d4ae12dbef73f
EdwinRojasVargas <edwinestudio72@gmail.com>
2021-03-30 20:50:49 -0500

    Acabando de hacer la verificacion del "register"

--- scripts/validar.js
+++ scripts/validar.js
@@ -1,21 +1,57 @@
-
+// Validacion de El Register
 function validar(){
     var nombre;
     var apellidos;
     var correo;
     var usuario;
-    var clave;
-    var confirmar_clave;
+    var contraseña;
+    var confirmar_contra;
     var expresion;
     nombre = document.getElementById("nombre").value;
     apellidos= document.getElementById("apellidos").value;
     correo = document.getElementById("correo").value;
     usuario = document.getElementById("usuario").value;
-    clave = document.getElementById("clave").value;
-    confirmar_clave = document.getElementById("confirmar_clave").value;
+    contraseña = document.getElementById("contraseña").value;
+    confirmar_contra = document.getElementById("confirmar_contra").value;
+    
+    expresion = /\w+@\w+\.+[a-z]/;
 
-    if(nombre === ""){
-        alert("El campo nombre esta vacio");
+    if(nombre === " " || apellidos === " " || correo === " " || usuario === " "
+    || contraseña === " "){
+        alert("Los campos de nombre, apellido, correo, usuario y contraseña son obligatorios");
+        return false;
+    }
+    else if(nombre.length>30) {
+        alert("El nombre es muy largo");
+        return false;
+    }
+    else if(apellidos.length>30) {
+        alert("Los apellidos son muy largo");
+        return false;
+    }
+    else if(correo.length>100) {
+        alert("El correo es muy largo");
+        return false;
+    }
+    else if(!expresion.test(correo)){
+        alert("El correo no es valido");
+        return false;
+    }
+    else if(usuario.length>20) {
+        alert("El usuario es muy largo");
+        return false;
+    }
+    else if(contraseña.length>20){
+        alert("La contraseña es muy larga");
+        return false;
+    }
+    else if(contraseña === confirmar_contra){
+        alert("Las contraseñas son iguales");
+        alert("!Bienvenido al Sistema!");
+    }
+    else{
+        alert("Las Contraseñas no son iguales, por favor intente de nuevo");
         return false;
     }
 }
+  
